42ad94aa8cb45120fc8b339aef63486ba7aacee9
HirushaR <hirusharandunu11@gmail.com>
2020-08-10 12:08:38 +0530

    Updates

--- js/count-down.js
+++ js/count-down.js
@@ -2,14 +2,16 @@ const second = 1000,
     minute = second * 60,
     hour = minute * 60,
     day = hour * 24;
+    month = day * 30;
 
-let countDown = new Date('Aug 15, 2020 00:00:00').getTime(),
+let countDown = new Date('Dec 20, 2020 00:00:00').getTime(),
     x = setInterval(function() {
 
         let now = new Date().getTime(),
             distance = countDown - now;
 
-        document.getElementById('days').innerText = Math.floor(distance / (day)),
+            document.getElementById('months').innerText = Math.floor(distance / (month)),
+            document.getElementById('days').innerText = Math.floor((distance/day) % 30),
             document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
             document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
             document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
